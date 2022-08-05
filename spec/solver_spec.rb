f3829af10c6cd90b0b8b3b7a109bb9d9ff9066b8
NatiG <nati.g5684@gmail.com>
2022-08-06 00:59:39 +0300

    Add test for negative values in factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,40 +1,49 @@
 require_relative 'spec_helper'
 
-require 'yaml'
-
 describe Solver do
   before :each do
     @solver = Solver.new
   end
-  it 'Create new instance of solver class' do
-    @solver.should be_an_instance_of Solver
+  describe 'factorial' do
+    it 'Create new instance of solver class' do
+      @solver.should be_an_instance_of Solver
+    end
+  
+    it 'Return the factorial of the number without breaking' do
+      expect(@solver.factorial(4)).to eql 24
+    end
+  
+    it 'Return 1 if the input is 0' do
+      expect(@solver.factorial(0)).to eql 1
+    end
+
+    it 'Raises an exception' do
+      # expect(@solver.factorial(-9)).to eql 
+      expect { @solver.factorial(-9) }.to raise_error('Sorry only positive number allowed')
+    end
   end
 
-  it 'Return the factorial of the number without breaking' do
-    expect(@solver.factorial(4)).to eql 24
+  describe 'reverse' do
+    it 'Should return the reversed version of the string' do
+      expect(@solver.reverse('hello')).to eql 'olleh'
+    end
   end
 
-  it 'Return 1 if the input is 0' do
-    expect(@solver.factorial(0)).to eql 1
-  end
-
-  it 'Should return the reversed version of the string' do
-    expect(@solver.reverse('hello')).to eql 'olleh'
-  end
-
-  it 'Should return fizz when nbr % 3 = 0' do
-    expect(@solver.fizzbuzz(3)).to eql 'fizz'
-  end
-
-  it 'Should return buzz when nbr % 5 = 0' do
-    expect(@solver.fizzbuzz(5)).to eql 'buzz'
-  end
-
-  it 'Should return fizzbuzz when nbr % 3 = 0 && nbr % 5 = 0' do
-    expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
-  end
-
-  it 'Should return nbr as a string else cases' do
-    expect(@solver.fizzbuzz(17)).to eql '17'
+  describe 'fizzbuzz' do
+    it 'Should return fizz when nbr % 3 = 0' do
+      expect(@solver.fizzbuzz(3)).to eql 'fizz'
+    end
+  
+    it 'Should return buzz when nbr % 5 = 0' do
+      expect(@solver.fizzbuzz(5)).to eql 'buzz'
+    end
+  
+    it 'Should return fizzbuzz when nbr % 3 = 0 && nbr % 5 = 0' do
+      expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
+    end
+  
+    it 'Should return nbr as a string else cases' do
+      expect(@solver.fizzbuzz(17)).to eql '17'
+    end
   end
 end
