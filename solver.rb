9bc3c31407b432a82c0676e68301b0cd39293794
bizip <bizip04@gmail.com>
2022-08-05 19:06:41 +0200

    Add a code for solver tdd

--- solver.rb
+++ solver.rb
@@ -1,1 +1,9 @@
-puts "hello pascal"
+class Solver
+    def factorial(nbr)
+        return 1 if nbr == 0
+        raise "Sorry only positive number allowed" if nbr.negative?
+        return nbr * factorial(nbr -1)
+    end
+end
+
+new = Solver.new
