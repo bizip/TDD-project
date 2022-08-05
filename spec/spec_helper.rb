b3e54b02fab9db2ee33b29f8517c2eec75bb1eeb
bizip <bizip04@gmail.com>
2022-08-05 18:58:08 +0200

    Add a test for factorial

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require_relative '../solver.rb'
+require 'yaml'
 
 describe Solver do
     before :each do
@@ -9,5 +10,15 @@ describe Solver do
         it 'Create new instance of solver class' do
         @solver.should be_an_instance_of Solver
         end
+    end
+
+    describe "#factorial" do
+        it "Return the factorial of the number without breaking" do
+           expect(@solver.factorial(4)).to eql 24
+        end
+
+        it "Return 1 if the input is 0" do
+            expect(@solver.factorial(0)).to eql 1
+         end
     end
 end
