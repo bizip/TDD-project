acf8223f22d6d80e6c8657724a58497a74e02c68
bizip <bizip04@gmail.com>
2022-08-05 19:13:47 +0200

    Fix the liners error

--- solver.rb
+++ solver.rb
@@ -1,9 +1,8 @@
 class Solver
-    def factorial(nbr)
-        return 1 if nbr == 0
-        raise "Sorry only positive number allowed" if nbr.negative?
-        return nbr * factorial(nbr -1)
-    end
-end
+  def factorial(nbr)
+    return 1 if nbr.zero?
+    raise 'Sorry only positive number allowed' if nbr.negative?
 
-new = Solver.new
+    nbr * factorial(nbr - 1)
+  end
+end
